6f8fd59a13327b6ac3a387bee56eba78ab3ceceb
mrdhockey87 <mrdhockey@gmail.com>
2026-01-21 15:35:56 -0600

    Version 3.0.0.1 Added notes to version 3.0.0.0  *  Version 3.0.0.0 Fix the dll not getting copied to the output directory. Need to fix the new backup  *                  should auto select system state when the boot voulume or disk is selected, also the selection for  *                  the disk or volume should be a explorer style tree view. The selection for the what too back up should be  *                  either check boxes or radio button group and should also include the Hyper-v virtual machines. (Maybe).  *                  The Hyper-V backups should be selectable without selecting any of the drives, volumes or files & folders.  *                  Restore should just give a Alert if there have been no backups run yet. The backup service manager should  *                  automatically install and should not be an option to install when the application is run. The service should not  *                  need a page to install, start stop and should be managed by the normal windows services mmc.

--- BackupUI/VersionClass.cs
+++ BackupUI/VersionClass.cs
@@ -47,22 +47,27 @@ namespace BackupUI
 				}
 				
 				// Last resort fallback
-				return "3.0.0.0";
+				return "3.0.0.1";
 			}
 			catch
 			{
 				// Fallback version if assembly version fails
-				return "3.0.0.0";
+				return "3.0.0.1";
 			}
 		}
 	}
 }
 
 /*
+ *  Version 3.0.0.1 Added notes to version 3.0.0.0
  *  Version 3.0.0.0 Fix the dll not getting copied to the output directory. Need to fix the new backup
  *                  should auto select system state when the boot voulume or disk is selected, also the selection for 
  *                  the disk or volume should be a explorer style tree view. The selection for the what too back up should be
  *                  either check boxes or radio button group and should also include the Hyper-v virtual machines. (Maybe).
+ *                  The Hyper-V backups should be selectable without selecting any of the drives, volumes or files & folders.
+ *                  Restore should just give a Alert if there have been no backups run yet. The backup service manager should 
+ *                  automatically install and should not be an option to install when the application is run. The service should not 
+ *                  need a page to install, start stop and should be managed by the normal windows services mmc. 
  *  Version 2.0.0.0 Fixed build errors that occurred after the AI built the first version.
  *  Version 1.0.0.0 added Version information for the application (This file) and had the AI write the app to run the backups
  *                  for windows servers and hyper-v virtual machines.
